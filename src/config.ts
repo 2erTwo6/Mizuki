39dcf11803e4401881fc4a2e9ae43f0ec0a3bbe3
2erTwo6 <2erTwo6@users.noreply.github.com>
2026-05-10 06:44:26 +0800

    Update site logo and favicon

--- src/config.ts
+++ src/config.ts
@@ -59,7 +59,7 @@ export const siteConfig: SiteConfig = {
 		// 顶栏标题图标路径，默认使用 public/assets/home/home.webp
 		icon: "assets/home/home.webp",
 		// 网站Logo图片路径
-		logo: "assets/home/default-logo.webp",
+		logo: "assets/home/default-logo.jpg",
 	},
 
 	// 页面自动缩放配置
@@ -184,12 +184,10 @@ export const siteConfig: SiteConfig = {
 	showCoverInContent: false, // 在文章内容页显示文章封面
 	generateOgImages: false, // 启用生成OpenGraph图片功能,注意开启后要渲染很长时间，不建议本地调试的时候开启
 	favicon: [
-		// 留空以使用默认 favicon
-		// {
-		//   src: '/favicon/icon.png',    // 图标文件路径
-		//   theme: 'light',              // 可选，指定主题 'light' | 'dark'
-		//   sizes: '32x32',              // 可选，图标大小
-		// }
+		{
+			src: '/assets/home/default-logo.jpg',
+			sizes: '640x640',
+		}
 	],
 
 	// 字体配置
